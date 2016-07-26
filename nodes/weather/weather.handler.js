d9c405939ef904ac8b7bec61db9d80f98c9ccb0a
Alexey <alexey@stuccomedia.com>
2016-07-26 10:19:46 +0300

    feat(weather) add config

--- nodes/weather/weather.handler.js
+++ nodes/weather/weather.handler.js
@@ -1,5 +1,6 @@
 var https     = require("https");
 const Handler = require('../../models/handler.model');
+const { city } = require('./weather.config');
 
 module.exports = new Handler({
   pattern                : /(^|\W)(погода|прогноз|(the )?weather|forecast)($|\W)/gi,
@@ -9,7 +10,7 @@ module.exports = new Handler({
     var req = https.request({
       host   : 'query.yahooapis.com',
       port   : 443,
-      path   : '/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22kiev%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+      path   : `/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`,
       method : 'GET'
     }, resp => {
       var output = [];
@@ -26,6 +27,8 @@ module.exports = new Handler({
         if ( obj.query && obj.query.count ) {
           let results = obj.query.results.channel;
           message.replyFormattedMessage(require('./weather.formatter').format(results));
+        } else {
+          message.replyText(`Could not find forecast for ${city}`);
         }
       });
 
